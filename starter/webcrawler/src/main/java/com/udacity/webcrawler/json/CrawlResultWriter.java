056cf2ed96201a525747aa14b820adebc510e0f4
Anoshko Alexey <anoshkoalexey@gmail.com>
2022-11-23 12:12:51 -0800

    Add StandardOpenOption.CREATE

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 /**
@@ -32,7 +33,7 @@ public final class CrawlResultWriter {
    */
   public void write(Path path) {
 
-    try (Writer writer = Files.newBufferedWriter(path)) {
+    try (Writer writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE)) {
       write(writer);
     } catch (Exception e) {
       e.printStackTrace();
